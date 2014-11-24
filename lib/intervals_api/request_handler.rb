0559061b716be69cb78b5854b22a0af48a3c6df7
aaron <alkrauss48@gmail.com>
2014-11-24 11:07:06 -0600

    Start allowing multiple options through in requests

--- lib/intervals_api/request_handler.rb
+++ lib/intervals_api/request_handler.rb
@@ -11,17 +11,23 @@ module IntervalsAPI
           'Accept'        => 'application/json',
           'Content-Type'  => 'application/json',
           'Authorization' => "Basic #{Base64.encode64(token + ":X")}"
-        }
+        },
+        query:  {},
+        body:   {}
       }
     end
 
     %w(get post put delete options).each do |request_type|
-      define_method request_type do |url, query = {}|
+      define_method request_type do |url, options = {}|
         RecursiveOpenStruct.new(
           self.class.send(
             request_type,
             url,
-            @default_options.merge({ body: query.to_json })
+            {
+              headers:  @default_options[:headers].merge( options[:headers].to_json),
+              query:    @default_options[:query].merge(   options[:query].to_json),
+              body:     @default_options[:body].merge(    options[:body].to_json)
+            }
           )
         )
       end
